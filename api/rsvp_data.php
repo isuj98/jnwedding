5186272b801732134a5ad6beb904e4c4a1b0a9e6
Justine Jusi <71004639+isuj98@users.noreply.github.com>
2025-06-09 14:47:07 +0800

    update

---
+++ api/rsvp_data.php
@@ -0,0 +1,9 @@
+[
+    {
+        "name": "test",
+        "contact": "12312",
+        "role": "Principal Sponsor",
+        "message": "asdasd",
+        "timestamp": "2025-06-09 08:08:27"
+    }
+]
